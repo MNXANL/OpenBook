6f28ebcc5bd568d72b33de93c4713d0f69486761
MNXANL <MNXANL@github.com>
2016-12-30 18:12:53 +0100

    He logrado ponerle el NavDrawer y los botones van bien! Pero la interacción por ahora es nula...

--- app/src/main/java/com/example/pr_idi/mydatabaseexample/MainActivity.java
+++ app/src/main/java/com/example/pr_idi/mydatabaseexample/MainActivity.java
@@ -3,12 +3,11 @@ package com.example.pr_idi.mydatabaseexample;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.widget.PopupMenu;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.PopupWindow;
 import android.widget.Toast;
 
 import java.util.List;
@@ -27,17 +26,13 @@ public class MainActivity extends ListActivity {
 
         List<Book> values = bookData.getAllBooks();
 
-        // use the SimpleCursorAdapter to show the
-        // elements in a ListView
+        // use the SimpleCursorAdapter to show the elements in a ListView
         ArrayAdapter<Book> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, values);
         setListAdapter(adapter);
     }
 
-    // Basic method to add pseudo-random list of books so that
-    // you have an example of insertion and deletion
-
-    // Will be called via the onClick attribute
-    // of the buttons in main.xml
+    // Basic method to add pseudo-random list of books so that you have an example of insertion and deletion
+    // Will be called via the onClick attribute of the buttons in main.xml
     public void onClick(View view) {
         @SuppressWarnings("unchecked")
         ArrayAdapter<Book> adapter = (ArrayAdapter<Book>) getListAdapter();
@@ -59,41 +54,41 @@ public class MainActivity extends ListActivity {
                     adapter.remove(book);
                 }
                 break;
-            case R.id.settngs_button:
-                // ...
-
-                // 1. Instantiate an AlertDialog.Builder with its constructor
-                AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
-
-                // 2. Chain together various setter methods to set the dialog characteristics
-                builder.setMessage(R.string.about_content).setTitle(R.string.about_title);
-
-                // 3. Get the AlertDialog from create()
-                AlertDialog dialog = builder.create();
-
+            case R.id.help:
+                //Aqui deberiamos crear alguna Activity aparte que solamente sea un Wall of Text (WoT)
+                Toast.makeText(MainActivity.this, "Halluda no se cmo fnsiona la pinxe aplicasion esta wey", Toast.LENGTH_SHORT).show();
                 break;
-            case R.id.about_button:
-                // En vez de usar Toast deberia ser una clase Dialog (con OK) o PopupWindow
-                Toast t = Toast.makeText(getApplicationContext(), "Developed by Marta & Miguel for IDI", Toast.LENGTH_LONG);
-                t.show();
+            case R.id.about:
+                //IT JUST WORKS!
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage(R.string.about_content).setTitle(R.string.about_title);
+                AlertDialog dialog = builder.create();
+                dialog.show();
+                break;
+            case R.id.button:
+                //Deberia mover este codigo Bethesda y ponerlo en la toolbar (que ni siquiera está)
+                DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+                drawer.openDrawer(GravityCompat.START);
+
                 break;
         }
-        adapter.notifyDataSetChanged();
+       adapter.notifyDataSetChanged();
     }
 
-    // Life cycle methods. Check whether it is necessary to reimplement them
 
+    // Life cycle methods. Check whether it is necessary to reimplement them
     @Override
     protected void onResume() {
         bookData.open();
         super.onResume();
     }
 
-    // Life cycle methods. Check whether it is necessary to reimplement them
 
+    // Life cycle methods. Check whether it is necessary to reimplement them
     @Override
     protected void onPause() {
         bookData.close();
+        Toast.makeText(MainActivity.this, "Pausing...", Toast.LENGTH_LONG).show();
         super.onPause();
     }
 
